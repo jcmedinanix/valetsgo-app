afe21bcb7837f25b9b3f7ab682ff85ee5cd0cfcd
Juan Carlos Medina Ruiz <cixcsistemas@costadelsolperu.com>
2026-06-02 16:50:25 -0500

    cambio de ejemplo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const VERSION = process.env.APP_VERSION || '1.3.0';
+const VERSION = process.env.APP_VERSION || '1.4.0';
 
 app.get('/', (req, res) => {
   res.send(`
